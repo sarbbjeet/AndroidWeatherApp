337a45d9b8793469888bb8192565ea7a8d81419b
sarbjeet <sarbjeets18@gmail.com>
2021-02-21 20:32:21 +0000

    add all files

---
+++ app/src/main/java/com/tees/ac/uk/a0321466/weatherapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.tees.ac.uk.a0321466.weatherapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
