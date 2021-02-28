b6e34a6e6709243be9a0cdccb0e7ac6aeba693cc
sarbjeet <sarbjeets18@gmail.com>
2021-02-28 10:27:55 +0000

    city button is active now

--- app/src/main/java/com/tees/ac/uk/a0321466/weatherapp/MainActivity.java
+++ app/src/main/java/com/tees/ac/uk/a0321466/weatherapp/MainActivity.java
@@ -3,12 +3,24 @@ package com.tees.ac.uk.a0321466.weatherapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    LinearLayout city_finder;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        city_finder= findViewById(R.id.cityFinder);
+        city_finder.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Toast.makeText(MainActivity.this,"button press",Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
