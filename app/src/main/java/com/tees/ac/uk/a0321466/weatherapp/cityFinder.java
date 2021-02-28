6ab3c5e5e4af99c0edaf1838d3dfcbf5b0f23d79
sarbjeet <sarbjeets18@gmail.com>
2021-02-28 13:46:05 +0000

    city_finder activity is active now

--- app/src/main/java/com/tees/ac/uk/a0321466/weatherapp/cityFinder.java
+++ app/src/main/java/com/tees/ac/uk/a0321466/weatherapp/cityFinder.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
@@ -14,17 +16,43 @@ public class cityFinder extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_city_finder);
 
+        //back button event
         ((ImageView)findViewById(R.id.goBack)).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(getApplicationContext(), MainActivity.class);
-                EditText city= findViewById(R.id.searchCity);
-                String _sCity= city.getText().toString();
-                i.putExtra("city", _sCity);
-                startActivity(i);
+               handleActivity(""); //go back without data
             }
         });
 
 
+        //cancel button
+        ((Button)findViewById(R.id.btnCancel)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handleActivity("");  //send empty string data
+            }
+        });
+            //ok button
+        ((Button)findViewById(R.id.btnOk)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText edCity=findViewById(R.id.searchCity);
+                handleActivity(edCity.getText().toString());  //pass enterted city name
+            }
+        });
+
+    }
+
+//    pass data from activity_city_finder to main activity
+     private void handleActivity(String s){
+        Intent i = new Intent(getApplicationContext(),MainActivity.class);
+        if(s !=""){
+            i.putExtra("city",s);
+        }
+        else {
+            i.putExtra("city", "");
+        }
+        startActivity(i);
     }
 }
